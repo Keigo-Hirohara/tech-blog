0c3286680eca7dd666a29db424c8ee7d1b20a2e6
Keigo-Hirohara <hirokei.camel@gmail.com>
2023-09-28 12:12:02 +0900

    create post page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,7 +1,20 @@
+import { getPostInfo } from '@/app/lib/getPostInfo';
 import React from 'react';
 
-const Post = () => {
-  return <div>this is a post page</div>;
+const Post = ({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}): JSX.Element => {
+  const { data, content } = getPostInfo(params.slug);
+  return (
+    <div className="postPage">
+      <h1>{data.title}</h1>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default Post;
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Link from 'next/link';
 import { PostCard } from '@/components/PostCard';
+import { getPostsInfo } from '../lib/getPostInfo';
 
 const Posts = () => {
-  const files = fs.readdirSync('_posts');
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-    const fileContent = fs.readFileSync(path.join('_posts', filename), 'utf-8');
-    const { data } = matter(fileContent);
-    return {
-      data,
-      slug,
-    };
-  });
+  const posts = getPostsInfo();
   return (
     <div>
       <h1>Posts</h1>
